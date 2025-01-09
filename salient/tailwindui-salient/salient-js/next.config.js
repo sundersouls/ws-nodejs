0dddfb77b1b4be092817eab353a06b89493f9901
sundersouls <k22570@list.ru>
2025-01-09 13:33:46 +0500

    2

--- salient/tailwindui-salient/salient-js/next.config.js
+++ salient/tailwindui-salient/salient-js/next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:3001/:path*',
-        },
-      ];
-    },
-  };
-  
+module.exports = nextConfig
